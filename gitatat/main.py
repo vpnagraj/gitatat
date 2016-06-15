167255a862c01c74519ecf8b76fa5bb238757bd2
VP Nagraj <vpnagraj@virginia.edu>
2016-06-15 16:15:48 -0400

    added gh-pages feature

--- gitatat/main.py
+++ gitatat/main.py
@@ -8,7 +8,7 @@ def main():
     parser =  argparse.ArgumentParser(prog='gitatat')
     
     parser.add_argument('input_file', default='.', nargs='?', help='file to be added')
-    parser.add_argument('--pages', dest = 'pages', action = 'store_true')
+    parser.add_argument('--pages', dest = 'pages', action = 'store_true', help = 'identify whether this repository contains a github pages branch to be updated')
     parser.set_defaults(pages = False)
     args = parser.parse_args()
     
@@ -23,13 +23,7 @@ def main():
     os.system(gitadd + " ; " + gitcommit + " ; " + gitpush)
     
     if args.pages:
-        os.system("git --version")
-    # TODO: include workflow for updating gh-pages 
-    # git branch -v | grep -c "gh-pages"
-    # git checkout gh-pages
-    # git rebase master
-    # git push origin gh-pages
-    # git checkout master
+        os.system("git checkout gh-pages; git rebase master; git push origin gh-pages; git checkout master")
     
 if __name__ == '__main__':
     main()
